f673b27e1e69c904c9a32f6fff004b0317c73e6c
daedaem <tg8685@gmail.com>
2023-02-18 01:14:19 +0900

    refactor: 배너 사진 추가 및 헤더, 모달 스타일링 수정

--- MealOrder/src/components/Header/Header.js
+++ MealOrder/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import Cart from "../Cart/Cart";
 import classes from "./Header.module.css";
 import Button from "../UI/Button";
-
+import headerImage from "../../assets/meals.jpg";
 const calcAmount = (props) => {
   let result = 0;
   props.map((el) => (result += el.amount));
@@ -28,24 +28,29 @@ const Header = (props) => {
   }, [props.orderList]);
 
   return (
-    <div className={classes.Header}>
-      <h1>ReactMeals</h1>
-      {modalOpen && (
-        <Cart
-          key={props.orderList.id}
-          orderList={props.orderList}
-          onClose={closeModal}
-          onOpen={openModal}
-        />
-      )}
-      <Button className={classes.modal_Button} onClick={openModal}>
-        <div className={classes.cart_buttons}>
-          <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
-          <p className={classes.cart_button_title}>Your Cart</p>
-          <p className={classes.cart_button_amount}>{cartInfos}</p>
-        </div>
-      </Button>
-    </div>
+    <>
+      <header className={classes.Header}>
+        <h1>ReactMeals</h1>
+        {modalOpen && (
+          <Cart
+            key={props.orderList.id}
+            orderList={props.orderList}
+            onClose={closeModal}
+            onOpen={openModal}
+          />
+        )}
+        <Button className={classes.modal_Button} onClick={openModal}>
+          <div className={classes.cart_buttons}>
+            <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
+            <p className={classes.cart_button_title}>Your Cart</p>
+            <p className={classes.cart_button_amount}>{cartInfos}</p>
+          </div>
+        </Button>
+      </header>
+      <div className={classes.header_main_image}>
+        <img className={classes.header_image} src={headerImage} alt="" />
+      </div>
+    </>
   );
 };
 
--- MealOrder/src/components/UI/Modal.js
+++ MealOrder/src/components/UI/Modal.js
@@ -1,4 +1,9 @@
+import classes from "./Modal.module.css";
 const Modal = (props) => {
-  return <section>{props.children}</section>;
+  return (
+    <section className={`${classes.Modal} + ${props.className}`}>
+      {props.children}
+    </section>
+  );
 };
 export default Modal;
